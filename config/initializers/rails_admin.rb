bc87a991e0bd8d9b0b7e8915acf6f5593bc27dc8
diml923 <seunggyu.im@gmail.com>
2016-08-09 04:57:31 +0900

    test

--- config/initializers/rails_admin.rb
+++ config/initializers/rails_admin.rb
@@ -1,7 +1,7 @@
 RailsAdmin.config do |config|
   config.authorize_with do
     authenticate_or_request_with_http_basic('Site Message') do |username, password|
-      username == 'foo' && password == 'bar'
+      username == 'admin' && password == 'chea'
     end
   end
 
